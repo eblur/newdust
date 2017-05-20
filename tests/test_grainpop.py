66da21132f2cbbbe5d8909bc0b064a5d163862a6
Lia Corrales <lia@space.mit.edu>
2017-05-20 16:13:43 +0200

    Moved extinction calculation tests to test_grainpop

--- tests/test_grainpop.py
+++ tests/test_grainpop.py
@@ -10,12 +10,20 @@ from . import percent_diff
 MD = 1.e-5  # g cm^-2
 RHO = 3.0   # g c^-3
 
+ALLOWED_SCATM = ['RG','Mie']
 TEST_SDIST = make_GrainDist('Powerlaw','Silicate', md=MD, rho=RHO)
 
+MRN_SIL = graindist.make_GrainDist('Powerlaw','Silicate')
+MRN_DRU = graindist.make_GrainDist('Powerlaw','Drude')
+EXP_SIL = graindist.make_GrainDist('ExpCutoff','Silicate')
+GRAIN   = graindist.make_GrainDist('Grain','Drude')
+
 NE, NA, NTH = 50, np.size(TEST_SDIST.a), 30
-THETA   = np.logspace(0, 4, NTH)
-LAMVALS = np.linspace(1000., 5000., NE)  # angs
-EVALS   = np.logspace(-1, 1, NE)  # kev
+THETA    = np.logspace(-10.0, np.log10(np.pi), NTH)  # 0->pi scattering angles (rad)
+ASEC2RAD = (2.0 * np.pi) / (360.0 * 60. * 60.)     # rad / arcsec
+TH_asec  = THETA / ASEC2RAD  # rad * (arcsec/rad)
+LAMVALS  = np.linspace(1000., 5000., NE)  # angs
+EVALS    = np.logspace(-1, 1, NE)  # kev
 
 # test that everything runs on both kev and angs
 test1 = SingleGrainPop(TEST_SDIST, 'Mie')
@@ -74,3 +82,36 @@ def test_make_MRN_drude():
     # Test that doubling the mass doubles the extinction
     test4 = make_MRN_drude(md=2.0*MD)
     assert percent_diff(test4.tau_ext, 2.0*test3.tau_ext) <= 0.01
+
+#-------- Test the extinction calculations
+
+# Test that all the computations can run
+@pytest.mark.parametrize(('gd','sm'),
+                         [(MRN_SIL, 'RG'), (MRN_SIL, 'Mie'),
+                          (MRN_DRU, 'RG'), (MRN_DRU, 'Mie'),
+                          (EXP_SIL, 'RG'), (EXP_SIL, 'Mie'),
+                          (GRAIN, 'RG'), (GRAIN, 'Mie')])
+def test_ext_calculations(gd, sm):
+    test = SingleGrainPop(gd, sm)
+    test.calculate_ext(LAMVALS, unit='angs', theta=TH_asec)
+    assert np.shape(test.tau_ext) == (NE,)
+    assert np.shape(test.tau_sca) == (NE,)
+    assert np.shape(test.tau_abs) == (NE,)
+    assert np.shape(test.diff) == (NE, len(gd.a), NTH)
+    assert all(percent_diff(test.tau_ext, test.tau_abs + test.tau_sca) <= 0.01)
+
+# Make sure that doubling the dust mass doubles the extinction
+@pytest.mark.parametrize('estring', ALLOWED_SCATM)
+def test_mass_double(estring):
+    gd1 = graindist.make_GrainDist('Powerlaw','Silicate')
+    gd2 = graindist.make_GrainDist('Powerlaw', 'Silicate')
+    gd2.md = 2.0 * gd1.md
+
+    gp1 = SingleGrainPop(gd1, estring)
+    gp1.calculate_ext(LAMVALS, unit='angs')
+    gp2 = SingleGrainPop(gd2, estring)
+    gp2.calculate_ext(LAMVALS, unit='angs')
+
+    assert all(percent_diff(gp2.tau_ext, 2.0 * gp1.tau_ext) <= 0.01)
+    assert all(percent_diff(gp2.tau_abs, 2.0 * gp1.tau_abs) <= 0.01)
+    assert all(percent_diff(gp2.tau_sca, 2.0 * gp1.tau_sca) <= 0.01)
