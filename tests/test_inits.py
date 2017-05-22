927774853dd8ad4cad51ed641aa334b38c6e1596
Lia Corrales <lia@space.mit.edu>
2017-05-22 03:12:04 +0200

    more advanced use of parametrize

--- tests/test_inits.py
+++ tests/test_inits.py
@@ -1,4 +1,4 @@
-
+import pytest
 from newdust import graindist
 from newdust import grainpop
 
@@ -9,13 +9,13 @@ ALLOWED_SCATM = ['RG','Mie']
 custom_sdist = graindist.sizedist.ExpCutoff(acut=0.5, nfold=12)
 custom_comp  = graindist.composition.CmDrude(rho=2.2)
 
-def test_graindist_init():
-    for size in ALLOWED_SIZES:
-        for comp in ALLOWED_COMPS:
-            test = graindist.GrainDist(size, comp)
-            assert isinstance(test, graindist.GrainDist)
-            assert test.size.dtype  == size
-            assert test.comp.cmtype == comp
+@pytest.mark.parametrize('sd', ALLOWED_SIZES)
+@pytest.mark.parametrize('cm', ALLOWED_COMPS)
+def test_graindist_init(sd, cm):
+    test = graindist.GrainDist(sd, cm)
+    assert isinstance(test, graindist.GrainDist)
+    assert test.size.dtype  == sd
+    assert test.comp.cmtype == cm
 
 def test_custom_graindist():
     test = graindist.GrainDist(custom_sdist, custom_comp, custom=True)
@@ -23,9 +23,9 @@ def test_custom_graindist():
     assert test.size.dtype == custom_sdist.dtype
     assert test.comp.cmtype == custom_comp.cmtype
 
-def test_grainpop_inits():
-    for size in ALLOWED_SIZES:
-        for comp in ALLOWED_COMPS:
-            for scat in ALLOWED_SCATM:
-                test = grainpop.SingleGrainPop(size, comp, scat)
-                assert isinstance(test, grainpop.SingleGrainPop)
+@pytest.mark.parametrize('sd', ALLOWED_SIZES)
+@pytest.mark.parametrize('cm', ALLOWED_COMPS)
+@pytest.mark.parametrize('sc', ALLOWED_SCATM)
+def test_grainpop_inits(sd, cm, sc):
+    test = grainpop.SingleGrainPop(sd, cm, sc)
+    assert isinstance(test, grainpop.SingleGrainPop)
