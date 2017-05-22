85ac35f3a54a34607dd8668c9b21e3782e43f612
Lia Corrales <lia@space.mit.edu>
2017-05-22 03:31:56 +0200

    tested custom SingleGrainPop.__init__

--- tests/test_inits.py
+++ tests/test_inits.py
@@ -17,15 +17,20 @@ def test_graindist_init(sd, cm):
     assert test.size.dtype  == sd
     assert test.comp.cmtype == cm
 
-def test_custom_graindist():
-    test = graindist.GrainDist(custom_sdist, custom_comp, custom=True)
-    assert isinstance(test, graindist.GrainDist)
-    assert test.size.dtype == custom_sdist.dtype
-    assert test.comp.cmtype == custom_comp.cmtype
-
 @pytest.mark.parametrize('sd', ALLOWED_SIZES)
 @pytest.mark.parametrize('cm', ALLOWED_COMPS)
 @pytest.mark.parametrize('sc', ALLOWED_SCATM)
 def test_grainpop_inits(sd, cm, sc):
     test = grainpop.SingleGrainPop(sd, cm, sc)
     assert isinstance(test, grainpop.SingleGrainPop)
+
+@pytest.mark.parametrize('sc', ALLOWED_SCATM)
+def test_custom_graindist(sc):
+    test = graindist.GrainDist(custom_sdist, custom_comp, custom=True)
+    assert isinstance(test, graindist.GrainDist)
+    assert test.size.dtype == custom_sdist.dtype
+    assert test.comp.cmtype == custom_comp.cmtype
+    test2 = grainpop.SingleGrainPop(custom_sdist, custom_comp, sc, custom=True)
+    assert isinstance(test2, grainpop.SingleGrainPop)
+    assert test2.size.dtype == custom_sdist.dtype
+    assert test2.comp.cmtype == custom_comp.cmtype
