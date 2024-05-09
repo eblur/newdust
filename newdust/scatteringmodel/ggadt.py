0b7d233f649a7606297f47633b61f9d85eb6c54b
gordonrl <gordonrl@umich.edu>
2024-05-09 12:48:08 -0400

    test

---
+++ newdust/scatteringmodel/ggadt.py
@@ -0,0 +1,8 @@
+'''
+What this file does:
+
+1. Reads GGADT output files and puts all of it into a FITS file of the same format as ScatteringModel
+2. Creates a ggadt version of the ScatteringModel class that can be initialized with a FITS file
+
+'''
+
